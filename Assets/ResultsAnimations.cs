501b01c6e3b88500f4a9c60f588d17597f772092
sugar studios <abcalkins8723@gmail.com>
2024-04-19 18:53:27 -0700

    added animations

--- Assets/ResultsAnimations.cs
+++ Assets/ResultsAnimations.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ProjectG
+namespace ProjectG.Results
 {
     public class ResultsAnimations : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private ResultsManager rManager;
+
+        private void Start()
         {
-        
+            rManager = GameObject.FindWithTag("ResultsManager").GetComponent<ResultsManager>();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void StartStatAnim()
         {
-        
+            StartCoroutine(WaitThenActivate(rManager.burgerMenu, 0));
+        }
+        public void StartBannerAnim()
+        {
+            StartCoroutine(WaitThenActivate(rManager.burgerBanner, 0));
+        }
+        public void StartButttonAnim()
+        {
+            StartCoroutine(WaitThenActivate(rManager.burgerButtons, 0.5f));
+        }
+
+        IEnumerator WaitThenActivate(GameObject obj, float time = 1f)
+        {
+            yield return new WaitForSeconds(time);
+            obj.SetActive(true);
         }
     }
 }
--- Assets/ResultsManager.cs
+++ Assets/ResultsManager.cs
@@ -1,15 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace ProjectG
 {
     public class ResultsManager : MonoBehaviour
     {
+
+        public GameObject burgerBanner;
+        public GameObject burgerMenu;
+        public GameObject burgerTitle;
+        public GameObject burgerButtons;
+
+        public GameObject resultsScreen;
+        public GameObject loadingScreen;
+
+        public Slider progressBar;
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            burgerTitle.SetActive(true);
+            burgerBanner.SetActive(false);
+            burgerMenu.SetActive(false);
+            burgerButtons.SetActive(false);
+        }
+
+        public void LoadLevel(string scene)
+        { 
+            StartCoroutine(LoadAsync(scene));
+        }
+
+        IEnumerator LoadAsync(string sceneName)
+        { 
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+            while (!operation.isDone)
+            {
+                progressBar.value = operation.progress;
+
+                yield return null;
+            }
         }
 
         // Update is called once per frame
