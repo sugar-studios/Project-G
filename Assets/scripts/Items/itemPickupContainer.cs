51c0bcc23ede114da18a2f5749fc13c5dfce6cfc
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2024-04-25 13:44:23 -0700

    item pickup

---
+++ Assets/scripts/Items/itemPickupContainer.cs
@@ -0,0 +1,12 @@
+using ProjectG.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectG
+{
+    public class itemPickupContainer : MonoBehaviour
+    {
+        public Item item;
+    }
+}
